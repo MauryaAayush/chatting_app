f35b75cd86cccadd6fb67fd3461708244a8df675
Admin <maayush235@gmail.com>
2024-08-13 23:43:35 +0530

    Radhey radhey

---
+++ lib/Model/user_model.dart
@@ -0,0 +1,30 @@
+class UserModel {
+
+  String? name, phone, email, photoUrl;
+
+  UserModel({
+    required this.name,
+    required this.phone,
+    required this.email,
+    required this.photoUrl,
+
+  });
+
+  factory UserModel.fromMap(Map m1){
+    return UserModel(name: m1['name'],
+        phone: m1['phone'],
+        email: m1['email'],
+        photoUrl: m1['photoUrl']);
+  }
+
+
+  Map<String, String?> toMap(UserModel user){
+    return {
+      'name' : user.name,
+      'phone' : user.phone,
+      'email' : user.email,
+      'photoUrl' : user.photoUrl,
+    };
+  }
+
+}
