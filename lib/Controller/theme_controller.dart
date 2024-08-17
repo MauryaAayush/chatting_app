e58e52ee17c96c6525df6b666f6f5a3253c54245
Admin <maayush235@gmail.com>
2024-08-17 23:02:51 +0530

    Radhey radhey

---
+++ lib/Controller/theme_controller.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../Theme/dark_theme.dart';
+import '../Theme/light_theme.dart';
+
+class ThemeController extends GetxController {
+  // Using a reactive variable
+  Rx<ThemeData> _themeData = lightTheme.obs;
+
+  // Getter for themeData
+  ThemeData get themeData => _themeData.value;
+
+  // Getter for isdark
+  bool get isDark => _themeData.value == darkTheme;
+
+  // Method to set the themeData
+  void setTheme(ThemeData themeData) {
+    _themeData.value = themeData;
+  }
+
+  // Method to toggle the theme
+  void toggleTheme() {
+    if (_themeData.value == lightTheme) {
+      _themeData.value = darkTheme;
+    } else {
+      _themeData.value = lightTheme;
+    }
+  }
+}
---
+++ lib/Views/setting_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../Controller/theme_controller.dart';
+
+
+class SettingScreen extends StatelessWidget {
+  const SettingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Access the ThemeController
+    final ThemeController themeController = Get.find<ThemeController>();
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        foregroundColor: Colors.grey,
+        centerTitle: true,
+        title: const Text('Settings'),
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(25),
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.secondary,
+            borderRadius: BorderRadius.circular(12)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text("Dark Theme"),
+            Obx(() => CupertinoSwitch(
+              value: themeController.isDark,
+              onChanged: (value) => themeController.toggleTheme(),
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
