fc9bbfc36e6c23a4619ec27aa65a984e6f88fe17
Admin <maayush235@gmail.com>
2024-08-21 00:03:55 +0530

    Radhey radhey

--- lib/Helper/google_services.dart
+++ lib/Helper/google_services.dart
@@ -1,27 +1,42 @@
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class GoogleLoginServices{
+class GoogleLoginServices {
   static GoogleLoginServices googleLoginServices = GoogleLoginServices._();
   GoogleLoginServices._();
 
   GoogleSignIn googleSignIn = GoogleSignIn();
   FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-
-  Future signInWithGoogle() async {
+  Future<void> signInWithGoogle() async {
     try {
-      final GoogleSignInAccount? googleSignInAccount = await googleSignIn
-          .signIn();
-      final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount!
-          .authentication;
+      final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
+      if (googleSignInAccount == null) {
+        print('User canceled the sign-in process.');
+        return;
+      }
+
+      final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
 
       final AuthCredential credential = GoogleAuthProvider.credential(
-          accessToken: googleSignInAuthentication.accessToken,
-          idToken: googleSignInAuthentication.idToken);
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
 
-      await firebaseAuth.signInWithCredential(credential);
+      final UserCredential userCredential = await firebaseAuth.signInWithCredential(credential);
+      final User? user = userCredential.user;
+
+      if (user != null) {
+        // Save user information to Firestore
+        await firestore.collection('users').doc(user.uid).set({
+          'name': user.displayName,
+          'email': user.email,
+          'phoneNumber': user.phoneNumber,
+          'photoURL': user.photoURL,
+        });
+      }
     } catch (e) {
       print(e.toString());
     }
@@ -39,8 +54,7 @@ class GoogleLoginServices{
   }
 
   void logOut() async {
-    googleSignIn.signOut();
-    firebaseAuth.signOut();
+    await googleSignIn.signOut();
+    await firebaseAuth.signOut();
   }
-
 }
