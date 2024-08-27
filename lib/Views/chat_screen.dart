0ef2f39ab272bf2a2fc42f9005ea8ee0e6b4fe21
Admin <maayush235@gmail.com>
2024-08-27 09:03:55 +0530

    Radhey radhey

--- lib/Views/chat_screen.dart
+++ lib/Views/chat_screen.dart
@@ -2,7 +2,6 @@ import 'package:chatting_app/Components/chat_container.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../Components/msg_textfield.dart';
 import '../Helper/auth service.dart';
 import '../Helper/chat_services.dart';
